b980a401d9bd3c26edb1975f79dd1848dfc31791
HUNTER <xujanov.xujamurod@gmail.com>
2025-03-13 16:34:01 +0500

    Modified Course

--- js/2 разница между arrays and objects.js
+++ js/2 разница между arrays and objects.js
@@ -1,7 +1,19 @@
 const arr  = [1, 2, 3]
 const arr3  = ['a', 'b', 'c']
 
+console.log(typeof('text')); //string
+console.log(typeof(2134)); 
+console.log(Array.isArray([]));
+console.log(Array.isArray({}));
+
+
 console.log(arr[1]);
+
+console.log('text' * 123); // NaN
+
+
+console.log(something);
+
 
 const arrObj = {
     0: 1,
@@ -9,14 +21,20 @@ const arrObj = {
     2: 3,
 }
 
+console.log(arrObj['2']);
+
+
 const arrObj2 = {
     0: 'a',
     1: 'b',
-    2: 'c',
+    2: [1, 2, 3],
 }
 
-const obj = {a: 1, b: 2};
 
+
+const obj = {a: [2, 3, 4], b: 2};
+
+console.log(typeof(Array.isArray(obj.a)) * 123); 
 
 
 const obj2  = {
@@ -25,8 +43,13 @@ const obj2  = {
 }
 
 
+// console.log(obj2.Anna);
+
+
 const arr2  = [
     'Anna', 500,
     'Alice', 1000,
 ]
+
+
 
